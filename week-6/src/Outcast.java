e90cac2ef4a17373d122726c6a5ddfdc5b1479b3
theluxury <zhengkaw@gmail.com>
2015-11-08 22:50:56 -0800

    nicer

--- week-6/src/Outcast.java
+++ week-6/src/Outcast.java
@@ -7,12 +7,13 @@ public class Outcast {
         this.wordNet = wordnet;
     }         // constructor takes a WordNet object
     public String outcast(String[] nouns) {
-        int maxDistance = Integer.MAX_VALUE;
+        int maxDistance = Integer.MIN_VALUE;
         String outcastCandidate = "";
         for (String noun : nouns) {
             int distance = getDistance(noun, nouns);
-            if (distance < maxDistance) {
+            if (maxDistance < distance) {
                 outcastCandidate = noun;
+                maxDistance = distance;
             }
         }
         return outcastCandidate;
@@ -21,13 +22,20 @@ public class Outcast {
     private int getDistance(String noun, String[] nouns) {
         int distance = 0;
         for (String otherNoun : nouns) {
-            if (noun != otherNoun) {
+            if (!noun.equals(otherNoun)) {
                 distance += wordNet.distance(noun, otherNoun);
             }
         }
         return distance;
     }
     public static void main(String[] args) {
+        WordNet wordNet = new WordNet("/Users/theluxury/Documents/algs/week-6/wordnet/synsets50000-subgraph.txt",
+                "/Users/theluxury/Documents/algs/week-6/wordnet/hypernyms50000-subgraph.txt");
+        Outcast outcast = new Outcast(wordNet);
+        String[] nouns = new String[] {"Asia", "Australia", "North_America", "India",
+                "Europe", "Antarctica", "South_America" };
+        System.out.print(outcast.outcast(nouns));
+
 
     }  // see test client below
 }
